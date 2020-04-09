954e71972ce53eee0ee2e794736368137283a1c8
Fernanda <fernandaromero85@gmail.com>
2020-04-09 17:42:12 -0500

    link sessions

--- app/javascript/src/login/signupWidget.jsx
+++ app/javascript/src/login/signupWidget.jsx
@@ -68,7 +68,7 @@ class SignupWidget extends React.Component {
       .then(data => {
         if (data.success) {
           const params = new URLSearchParams(window.location.search);
-          const redirect_url = params.get('redirect_url') || '/';
+          const redirect_url = params.get('redirect_url') || '/tweets';
           window.location = redirect_url;
         }
       })
